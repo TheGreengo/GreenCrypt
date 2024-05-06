2beaadd114e1341ba5c52744569f06b6a35216e6
Braden <bradenoptimus@gmail.com>
2024-05-05 19:42:57 -0700

    huh,

--- green_crypt.go
+++ green_crypt.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
 func main() {
-	
+	file_ptr := flag.String("f","", "The name of the file to be encrypted")
+	dec_ptr := flag.Bool("d", false, `Flag to indicate if flag is being decrypted 
+	default (false) means that file is to be encrypted`)	
+
+	flag.Parse()
+
+	fmt.Println("File name:", *file_ptr, "\nDecrypt:", *dec_ptr)	
 }
