00f35308f834dfe92638e66e5a60a6e0eed83a6a
Braden <bradenoptimus@gmail.com>
2024-05-09 21:11:26 -0700

    stuff

--- green_crypt.go
+++ green_crypt.go
@@ -9,8 +9,9 @@ import (
 
 func main() {
 	file_ptr := flag.String("f","", "The name of the file to be encrypted")
-	// dec_ptr := flag.Bool("d", false, `Flag to indicate if flag is being decrypted 
-	// default (false) means that file is to be encrypted`)	
+	dec_ptr := flag.Bool("d", false, `Flag to indicate if flag is being decrypted
+	 default (false) means that file is to be encrypted`)	
+	key_ptr := flag.String("k", "secret", "The key to be used to encrypt the file")
 
 	flag.Parse()
 
@@ -21,13 +22,19 @@ func main() {
 
 	br := bufio.NewReader(file)
 
+	var new_bits []byte
 	for {
 		b, err := br.ReadByte() 
 		
 		if (err != nil) {
 			break
 		}
-
+		
+		append(new_bits, b + ind)
+		ind = (ind + 1) % len(*key_ptr)
 		fmt.Printf("%c", b)
+		fmt.Printf("%d", b + ind)
 	}
+	
+	os.WriteFile(*file_ptr, new_bits, 0644)
 }
